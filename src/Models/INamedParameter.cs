a4c911595a2ea226aff99ffff1b0d2d23e8b4640
ErikWe <erikwestberg7@gmail.com>
2024-08-06 14:04:14 +0200

    Derive from parameter marker-interface

--- src/Models/INamedParameter.cs
+++ src/Models/INamedParameter.cs
@@ -1,7 +1,10 @@
 ﻿namespace Paraminter.Parameters.Named.Models;
 
+using Paraminter.Associators.Models;
+
 /// <summary>Represents a named parameter.</summary>
 public interface INamedParameter
+    : IParameter
 {
     /// <summary>The name of the named parameter.</summary>
     public abstract string Name { get; }
